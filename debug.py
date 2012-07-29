b849e6a792f10de2c2d358848e9a40a0f8703df6
rembo10 <rembo10@headphones>
2012-07-29 23:28:09 +0530

    Added locale encoding/decoding to debug.py

--- debug.py
+++ debug.py
@@ -15,10 +15,34 @@
 #  along with Headphones.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import locale
 
 from lib.beets.mediafile import MediaFile
 
 music_dir = raw_input("Enter the path to your music directory: ")
+
+music_dir = unicode(music_dir)
+
+# Try to mimic the encoding/decoding done in the original librarysync
+SYS_ENCODING = None
+
+try:
+    locale.setlocale(locale.LC_ALL, "")
+    SYS_ENCODING = locale.getpreferredencoding()
+    print "SYS_ENCODING is: " + SYS_ENCODING
+except (locale.Error, IOError):
+    print "Could not get SYS_ENCODING"
+    pass
+
+# for OSes that are poorly configured I'll just force UTF-8
+if not SYS_ENCODING or SYS_ENCODING in ('ANSI_X3.4-1968', 'US-ASCII', 'ASCII'):
+    SYS_ENCODING = 'UTF-8'
+    
+print "Configured SYS_ENCODING is: " + SYS_ENCODING
+
+music_dir = music_dir.encode(SYS_ENCODING)
+print "Encoded music_dir is " + repr(music_dir)
+raw_input("Press Enter to start scan\n")
     
 for r,d,f in os.walk(music_dir):
     for files in f:
@@ -31,7 +55,7 @@ for r,d,f in os.walk(music_dir):
                 f = MediaFile(song)
 
             except:
-                print 'Cannot read file: ' + song
+                print 'Cannot read file: ' + song.decode(SYS_ENCODING)
                 continue
                 
             print f.bitrate
